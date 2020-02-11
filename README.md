2ece2ba84744be7db0e7d809318ca3fc8d0be968
Den Sprous <dennis.x.sprous.-nd@disney.com>
2020-02-11 10:21:31 -0800

    simple docker wrapper around ubuntu/anaconda

---
+++ README.md
@@ -0,0 +1,10 @@
+## Docker: setup ubuntu and run anaconda3
+
+Docker file to create an image Ubuntu/Anaconda3 with a simple 'hello world' style python script to show everything works.  
+
+# To do this:
+
+docker build -t docker_anaconda3_wrapper.docker_image .
+docker run docker_anaconda3_wrapper.docker_image
+
+ 
---
+++ piethein_grooks.py
@@ -0,0 +1,15 @@
+# Author:  Dennis Sprous
+# Created: 11-Feb-2020
+# Purpose: Hello world it too boring.  
+#          Piet Hein's Grooks comforts the soul
+#           
+
+def main():
+    print( '''
+           Any problem        worthy of attack
+           proves its worth   by fighting back
+               Piet Heins
+           ''' )
+
+main()
+
